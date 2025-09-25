dd89e0f82e93da4bf94d3ca1964c351f81036331
cinostko <sergiorojas0123@gmail.com>
2025-09-24 23:05:06 -0500

    Ejercicio 1, práctica de figuras

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Programacion_Semana1_Ejercicio
+{
+    class rectangulo
+    {
+        private double _base;
+        private double _altura;
+
+        public rectangulo(double b, double a)
+        {
+            _base = b;
+            _altura = a;
+        }
+
+        public double calcularArea()
+        {
+            return _base + _altura;
+        }
+    }
+
+    class circulo
+    {
+        private double _radio;
+
+        public circulo(double r)
+        {
+            _radio = r;
+        }
+
+        public double calcularArea()
+        {
+            double pi = 3.1416;
+            return pi * _radio * _radio;
+        }
+    }
+
+    class Triangulo
+    {
+        private double _base;
+        private double _altura;
+
+        public Triangulo(double b, double a)
+        {
+            _base = b;
+            _altura = a;
+        }
+
+        public double CalcularArea()
+        {
+            return (_base * _altura) / 2;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.WriteLine("Seleccione una figura para calcular el areaa:");
+                Console.WriteLine("1. rectángulo");
+                Console.WriteLine("2. círculo");
+                Console.WriteLine("3. triángulo");
+                Console.Write("Opción: ");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.Write("ingresar la base del rectángulo: ");
+                        double baseRect = Convert.ToDouble(Console.ReadLine());
+
+                        Console.Write("ingresar la altura del rectángulo: ");
+                        double alturaRect = Convert.ToDouble(Console.ReadLine());
+
+                        rectangulo rect = new rectangulo(baseRect, alturaRect);
+                        Console.WriteLine("area, del rectángulo: " + rect.calcularArea());
+                        break;
+
+                    case "2":
+                        Console.Write("ingresar el radio del círculo: ");
+                        double radio = Convert.ToDouble(Console.ReadLine());
+
+                        circulo circ = new circulo(radio);
+                        Console.WriteLine("area del círculo: " + circ.calcularArea());
+                        break;
+
+                    case "3":
+                        Console.Write("ingresar la base del triángulo: ");
+                        double baseTri = Convert.ToDouble(Console.ReadLine());
+
+                        Console.Write("ingresar la altura del triángulo: ");
+                        double alturaTri = Convert.ToDouble(Console.ReadLine());
+
+                        Triangulo tri = new Triangulo(baseTri, alturaTri);
+                        Console.WriteLine("area del triángulo: " + tri.CalcularArea());
+                        break;
+
+                    default:
+                        Console.WriteLine("opcion invalida intenta nuevamente.");
+                        break;
+                }
+
+                Console.Write("\n¿quieres calcular otra figura? (s/n): ");
+                string respuesta = Console.ReadLine().ToLower();
+
+                if (respuesta != "s")
+                {
+                    continuar = false;
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Programa finalizadoo :D . Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Programacion-Semana1-Ejercicio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Programacion-Semana1-Ejercicio")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("b0dcd425-2b22-461f-9680-90e079f0f501")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
